c2efb0b235f707833e8900cce7270b54d18cb7d8
hpeng <207667803@qq.com>
2021-11-25 01:45:03 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/per/hp/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package per.hp.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
